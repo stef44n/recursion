4fb276cff3942f85014805ab44886e491a7aa748
Stefan <stefanseifeddine@gmail.com>
2022-10-26 02:44:53 +0100

    Add solution to assignment 1.2

--- assignment1.js
+++ assignment1.js
@@ -16,27 +16,20 @@ const fibs = function (num) {
     return init;
 };
 
-// const fibsRec = function (num) {
-//     let array = [0, 1];
-//     if (num == 1) {
-//         return array;
-//     } else if (num == 2) {
-//         return array;
-//     }
-
-//     // while (num > 2) {
-//     let num1 = array[-1];
-//     let num2 = array[-2];
-//     console.log(num);
-//     console.log(array[-1]);
-//     console.log(array[-2]);
-//     // }
-// };
-
-console.log(fibs(0));
-console.log(fibs(1));
-console.log(fibs(2));
-console.log(fibs(3));
 console.log(fibs(8));
 
-// console.log(fibsRec(8));
+const array = [0, 1];
+const fibsRec = function (num) {
+    if (num < 3) {
+        return array;
+    } else {
+        let num1 = array.length - 2;
+        let num2 = array.length - 1;
+        let newNum = Number(array[num1] + array[num2]);
+        array.push(newNum);
+        fibsRec(num - 1);
+    }
+    return array;
+};
+
+console.log(fibsRec(8));
